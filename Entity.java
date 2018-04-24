a4c2a8bcd7d033a3a1c60d3590695e35b18773a3
jxmaymay <31222311+jxmaymay@users.noreply.github.com>
2018-04-24 10:36:05 -1000

    Add files via upload

---
+++ Entity.java
@@ -0,0 +1,19 @@
+
+public class Entity {
+	
+	private int hp;
+	private int atk;
+	private int def;
+	private int spA;
+	private int spD;
+	private int spe;
+	
+	public Entity(int h, int a, int d, int sA, int sD, int sp) {
+		hp = h;
+		atk = a;
+		def = d;
+		spA = sA;
+		spD = sD;
+		spe = sp;
+  }
+}
---
+++ PlayerChar.java
@@ -0,0 +1,10 @@
+
+public class PlayerChar extends Entity {
+	
+	
+	public PlayerChar(int h, int a, int d, int sA, int sD, int sp) {
+		super(h, a, d, sA, sD, sp);
+	}
+	
+	public void 
+}
